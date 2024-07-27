6ef3a7796408569204260e060f4d7d8982d3ff52
Benjamin <benlenormand61@gmail.com>
2024-07-27 18:19:57 +0200

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,142 @@
-import './App.css'
-import io from 'socket.io-client'
-import {useEffect, useState} from "react";
+import { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import './App.css';
 
-const socket = io('http://localhost:3001')
+const socket = io('http://localhost:3001');
+
 function App() {
+    const [roomName, setRoomName] = useState('');
+    const [username, setUsername] = useState('');
     const [message, setMessage] = useState('');
-    const [messageReceived, setMessageReceived] = useState('');
-    const [roomNumber, setRoomNumber] = useState('');
-    const [pseudo, setPseudo] = useState('');
-    const [users, setUsers] = useState([]);
+    const [messages, setMessages] = useState([]);
+    const [inRoom, setInRoom] = useState(false);
+    const [roomData, setRoomData] = useState({ users: [], messages: [] });
+    const [rooms, setRooms] = useState([]);
+    const [privateRoom, setPrivateRoom] = useState(false);
 
-    const sendMessage = () => {
-        socket.emit('send_message', { message, roomNumber, pseudo });
-    }
+    useEffect(() => {
+        const fetchRooms = async () => {
+            const response = await fetch('http://localhost:3001/rooms');
+            const data = await response.json();
+            console.log(data);
+            setRooms(data);
+        }
+        fetchRooms();
+    }, []);
 
-    const joinRoom = () => {
-        if (roomNumber !== "") {
-            socket.emit('join_room', {roomNumber, pseudo});
-        } else {
-            alert('Veuillez renseigner un numéro de room');
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages((prevMessages) => [...prevMessages, message]);
+        });
+
+        socket.on('roomData', (room) => {
+            console.log('Room data:', room);
+            setRoomData(room);
+        });
+
+        return () => {
+            socket.off('message');
+            socket.off('roomData');
+        };
+    }, []);
+
+    const createRoom = (roomName) => {
+        if (roomName && username) {
+            socket.emit('createRoom', { roomName, username, privateRoom });
+            setInRoom(true);
+        } else if (roomName && !username) {
+            alert('Please enter a username');
         }
     }
 
-    const disconnect = () => {
-        alert(`Vous avez été déconnecté de la room ${roomNumber}`);
-        setRoomNumber('');
-        socket.emit('leave', {roomNumber});
-    }
+    const joinRoom = (roomName) => {
+        if (roomName && username) {
+            socket.emit('joinRoom', { roomName, username });
+            setInRoom(true);
+            setRoomName(roomName);
+        } else if (roomName && !username) {
+            alert('Please enter a username');
+        }
+    };
 
-    useEffect(() => {
-        socket.on('receive_message', (msg) => {
-            setMessageReceived(msg.message);
-        });
-        socket.on('user_connect', (data) => {
-            console.log(`${data.pseudo} a rejoint la room`);
-            setUsers([...users, data.pseudo]);
-        });
-        socket.on('user_disconnect', (data) => {
-            console.log(`${data.pseudo} a quitté la room`);
-            setUsers(users.filter(user => user !== data.pseudo));
-        });
-    }, [socket]);
+    const leaveRoom = () => {
+        socket.emit('leaveRoom', { roomName, username });
+        setInRoom(false);
+        setMessages([]);
+    };
 
-  return (
-    <>
-        <input placeholder='pseudo' onChange={(e) => setPseudo(e.target.value)}/>
-        <input placeholder='Room Number' onChange={(e) => setRoomNumber(e.target.value)}/>
-        <input placeholder='message' onChange={(e) => setMessage(e.target.value)}/>
-        <button onClick={sendMessage}>Envoyer</button>
-        <button onClick={joinRoom}>Rejoindre la room</button>
-        <button onClick={disconnect}>Déconnecter</button>
-        <p>{messageReceived}</p>
-        <h1>Liste des utilisateurs</h1>
-        <ul>
-            {users.map((user, index) => {
-                return <li key={index}>{user}</li>
-            })}
-        </ul>
-    </>
-  )
+    const sendMessage = () => {
+        if (message) {
+            socket.emit('sendMessage', { roomName, username, text: message });
+            setMessage('');
+        }
+    };
+
+    return (
+        <div className="App">
+            <h1>Chat Room</h1>
+            {!inRoom ? (
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Créer une room"
+                        value={roomName}
+                        onChange={(e) => setRoomName(e.target.value)}
+                    />
+                    <div>
+                        <label>Private Room</label>
+                        <input type={"checkbox"} onChange={() => setPrivateRoom(!privateRoom)}/>
+                    </div>
+                    <input
+                        type="text"
+                        placeholder="Enter your username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <button onClick={() => createRoom(roomName)}>Créer ma room</button>
+                </div>
+            ) : (
+                <div>
+                    <button onClick={leaveRoom}>Leave Room</button>
+                    <ul id="messages">
+                        {messages.map((msg, index) => (
+                            <li key={index}>
+                                {new Date(msg.timestamp).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })} <b>{msg.username}</b>: <b>{msg.text}</b>
+                            </li>
+                        ))}
+                    </ul>
+                    <input
+                        type="text"
+                        placeholder="Enter message"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                    />
+                    <button onClick={sendMessage}>Send Message</button>
+                </div>
+            )}
+            {inRoom && (
+                <div>
+                    <h2>Users in room:</h2>
+                    <ul>
+                        {roomData && roomData.users.map((user, index) => (
+                            <li key={index}>{user.username} : {user.points}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+            <h2>Rooms:</h2>
+            <ul>
+                {rooms && rooms.map((room, index) => (
+                    !room.private && (
+                        <div key={index} style={{display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "row", backgroundColor: "red", cursor: "pointer", margin: "0 0 2em 0"}} onClick={() => joinRoom(room.roomId)}>
+                            <p>{room.roomName}</p>
+                            <p>{room.users.length}</p>
+                        </div>
+                    )
+                ))}
+            </ul>
+        </div>
+    );
 }
 
-export default App
+export default App;
